35b25436767467c18104ee1988ba78e931be10c4
abblejuice <43124592+abblejuice@users.noreply.github.com>
2018-12-09 22:20:26 -0500

    Part 3

---
+++ Assets/Scripts/QuitOnEscape.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnEscape : MonoBehaviour
+{
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
