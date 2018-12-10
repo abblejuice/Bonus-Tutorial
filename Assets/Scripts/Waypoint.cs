a32d93750a78ba80f8ed720b7b6c6bb266083d9f
abblejuice <43124592+abblejuice@users.noreply.github.com>
2018-12-09 19:37:21 -0500

    Part 1

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float debugDrawRadius = 1.0F;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+    }
+}
